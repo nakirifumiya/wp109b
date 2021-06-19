0380fb38d43e2530354bf8ebdb6238f0036dda2e
nakirifumiya <80063674+nakirifumiya@users.noreply.github.com>
2021-06-20 02:30:15 +0800

    Update 01.js

--- hw/01.js
+++ hw/01.js
@@ -1,11 +1,9 @@
-## 作業 2
+作業 2
 請寫一個程式可以印出指定數量的 * 號
 例如： star(5) 會印出 5 個 * 號
 
   
 star(5)
-
----
 
 請寫一個函數可以印出 a..b 之間的所有整數
 例如： between(3,8) 會印出
@@ -13,8 +11,8 @@ star(5)
   
 3 4 5 6 7 8
 
----
-```html
+
+
 function st(n) {
     for (var i=0; i<n; i++)
     console.log('*')
@@ -25,4 +23,4 @@ for (var i=x; i<=y; i++ )
      console.log(i)
 }
 bt(3,8)
-```
+
